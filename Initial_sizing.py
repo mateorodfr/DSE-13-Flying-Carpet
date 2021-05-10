866a432b7a1e28d908c1a2fbb365c0dfd73d8b10
jaslolxD <57628181+jaslolxD@users.noreply.github.com>
2021-05-10 17:58:01 +0200

    ey

--- Initial_sizing.py
+++ Initial_sizing.py
@@ -1,39 +1,56 @@
 import numpy as np
 g=9.81
 Maxpayload= 600
+Missiont= 45/60
 
 #Engine
 Nengines= 4
-BladeD= np.arange(2.5,5.25,0.25)
+BladeD=  np.arange(2.5,5.25,0.25)
 RPM= 650
 Torque= 1500
-Power= 204000
-CL= 1.5                                    #assumption NACA-2412
-Nblade= 5
+Power= 104000
+CL= 1.1                                    #assumption NACA-2412
+Nblade= 6
 Sb= 0.1* BladeD**2 * Nblade
 Vtip= 0.5*BladeD * 2* np.pi * RPM/60
+Prop_eff= 0.85
+M_eng= 49
 
 #Reynolds number
 rho= 1.225
 V= Vtip / np.sqrt(2) #Change np.sqrt(2)
 C= 0.1* BladeD
 visc= 1.46*10**-5
-
 Re= rho*V*C/visc
 
-
-T= Vtip**2 /6 * rho * CL * Sb
+#Thrust per engine
+T= Vtip**2 /6 * rho * CL * Sb *Prop_eff
 print(T * Nengines /9.81)
 
 
 
-
-Thrust1= 600* 1.5  *9.81
-
-P_T_ratio= 70000/1912.73529
-P_W_ratio= 204000/49/9.81
-
+#Battery stuff
 Bat_E_dense=250 #Wh/kg
-Bat_P_dense=750 #W/kg
+Bat_P_dense=500 #W/kg
+Bat_eff=0.9
+Eng_eff=0.85
+
+#Power required
+P_req= Power*Nengines *1.2
+M_bat= P_req* Missiont/ Bat_E_dense /Bat_eff / Eng_eff
+#print(M_bat)
 
 
+Mass2= M_bat +Maxpayload + Nengines* M_eng + 1.5* Maxpayload
+
+Bat_P= M_bat * Bat_P_dense
+
+Bat_P_dense2= P_req/ M_bat
+
+
+
+M_bat2= P_req / Bat_P_dense /Bat_eff /Eng_eff
+
+M_total= 1.5*(Mass2+M_bat2)
+print(M_bat2+M_bat)
+print("Total mass", M_total)
