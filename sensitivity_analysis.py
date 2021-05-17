903c062739c915dd2299946f88ec0a40f5730400
mrdegraaf <M.R.deGraaf@student.tudelft.nl>
2021-05-17 14:56:12 +0200

    added weight estimation to tradeoff sensitivity analysis

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -3,25 +3,34 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read data from csv format
-data = pd.read_csv('sensitivity.csv', delimiter='\t').dropna()
 
-# create plots
-fig, ax = plt.subplots(figsize=(8, 6))
+def plot_boxplot(filename, save=False):
+    """Plots a boxplot based on trade-off table data"""
+    # read data from csv format
+    data = pd.read_csv(filename, delimiter='\t').dropna()
 
-bp = ax.boxplot(data, patch_artist=True)
-for patch in bp['boxes']:
-    patch.set(facecolor='cyan')
+    # create plots
+    fig, ax = plt.subplots(figsize=(8, 6))
 
-ax.set_ylabel('Trade-off Score')
-major_ticks = np.linspace(2, 4, 5)
-minor_ticks = np.linspace(2, 4, 25)
-ax.set_yticks(major_ticks)
-ax.set_yticks(minor_ticks, minor=True)
-ax.grid(which='minor', alpha=0.2)
-ax.grid(which='major', alpha=0.5)
-ax.set_xticklabels(list(data.columns), rotation=30)
-fig.tight_layout()
+    bp = ax.boxplot(data, patch_artist=True)
+    for patch in bp['boxes']:
+        patch.set(facecolor='cyan')
 
-# plt.show()
-plt.savefig(r"figures/sensitivity.pdf")
+    ax.set_ylabel('Trade-off Score')
+    major_ticks = np.linspace(2, 4, 5)
+    minor_ticks = np.linspace(2, 4, 25)
+    ax.set_yticks(major_ticks)
+    ax.set_yticks(minor_ticks, minor=True)
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+    ax.set_xticklabels(list(data.columns), rotation=30)
+    fig.tight_layout()
+
+    if save:
+        plt.savefig(r"figures/sensitivity.pdf")
+    else:
+        plt.show()
+
+if __name__ == "__main__":
+    plot_boxplot("sensitivity.csv")
+    plot_boxplot("sensitivity_new.csv", save=True)
