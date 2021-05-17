2ee172b150d1bedcae767df1014c5e90ffe0a0af
mateorodfr <62598297+mateorodfr@users.noreply.github.com>
2021-05-17 11:05:43 +0200

    Update Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -25,11 +25,12 @@ class MotorParameters(object):
         self.Torque = 1500
         self.P_max = 204000
         self.M_motor = 49
+        self.eff_motor = 0.95
         self.SF_rotational = 0.8
 
     def getMotorParameters(self,printParameters=False):
 
-        self.Parameters_motor = [self.Name_motor, self.N_motor,self.R_motor,self.Torque,self.P_max, self.M_motor, self.SF_rotational]
+        self.Parameters_motor = [self.Name_motor, self.N_motor,self.R_motor,self.Torque,self.P_max, self.M_motor, self.eff_motor, self.SF_rotational]
         if printParameters:
             print(f'The current motor parameters are:'
             + f'\n\tName: {self.Parameters_motor[0]} [-]'
@@ -38,30 +39,32 @@ class MotorParameters(object):
             + f'\n\tMaximum Motor Torque: {self.Parameters_motor[3]} [Nm]'
             + f'\n\tMaximum Motor Power: {self.Parameters_motor[4]/1000} [kW]'
             + f'\n\tMass of Motor: {self.Parameters_motor[5]} [kg]'
-            + f'\n\tSafety Factor for Motor Moment of Inertia: {self.Parameters_motor[6]} [-]'
+            + f'\n\tEfficiency of Motor: {self.Parameters_motor[6]} [-]'
+            + f'\n\tSafety Factor for Motor Moment of Inertia: {self.Parameters_motor[7]} [-]'
             + f'\nThe variables in the output array appear in this order.'
             )
         return self.Parameters_motor
 
 motor = MotorParameters()
+print(motor.getMotorParameters(True))
 
+def PropellerParameters(object):
 
+    """
+    PROPELLERS
+    CL: Lift coefficient The cross section is assumed to be constant throughout the blade and the airfoil applied is NACA-2412
+        CL determined from airfoiltools by taking the heighest Reynolds number
+    N_blade: The blade count is assumed for now. No papers were found which derive the optimal number. Also,
+             it is not clear whether the amount of blades reduces the efficiency of the lift generation properties
+    D_blade: Various diameters of the propellers are inspected to determine the best fit
+    W_blade: The width of the blade is assumed to be a tenth of the length. Could search for papers on propeller design
+    t_blade: Thickness of the blade assumed to be 1% of length on average
+    rho_blade: density of the propeller blades
+    """
 
-        # """
-        # PROPELLERS
-        # CL: Lift coefficient The cross section is assumed to be constant throughout the blade and the airfoil applied is NACA-2412
-        #     CL determined from airfoiltools by taking the heighest Reynolds number
-        # N_blade: The blade count is assumed for now. No papers were found which derive the optimal number. Also,
-        #          it is not clear whether the amount of blades reduces the efficiency of the lift generation properties
-        # D_blade: Various diameters of the propellers are inspected to determine the best fit
-        # W_blade: The width of the blade is assumed to be a tenth of the length. Could search for papers on propeller design
-        # t_blade: Thickness of the blade assumed to be 1% of length on average
-        # rho_blade: density of the propeller blades
-        # """
-
-        # self.CL = 1.3
-        # self.N_blade = 6
-        # self.D_blade = 2.5
-        # self.W_blade = 0.1 * self.D_blade
-        # self.t_blade = 0.01 * self.D_blade / 2
-        # self.rho_blade = 660
+    self.CL = 1.3
+    self.N_blade = 6
+    self.D_blade = 2.5
+    self.W_blade = 0.1 * self.D_blade
+    self.t_blade = 0.01 * self.D_blade / 2
+    self.rho_blade = 660
