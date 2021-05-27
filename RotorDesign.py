95ccf22bfd9b40018fb0909dd24ebc0831d8d2dc
mrdegraaf <M.R.deGraaf@student.tudelft.nl>
2021-05-27 17:45:03 +0200

    new aero data for naca23018

--- RotorDesign.py
+++ RotorDesign.py
@@ -3,15 +3,19 @@ from xfoil.model import Airfoil
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import sys
 
 xf = XFoil()
 
 def read_airfoil(filename):
     """Opens a file in the format of two columns and creates Airfoil object"""
     data = np.genfromtxt(filename, skip_header=1).T
-    x_arr, y_arr = data[0], data[1]
-    return Airfoil(x_arr, y_arr)
+    print(data)
+    bad = np.isnan(data).any() # TODO fix check to find NaN values
+    if not bad:
+        x_arr, y_arr = data[0], data[1]
+        return Airfoil(x_arr, y_arr)
+    else:
+        raise ValueError("airfoil file contains NaN")
 
 def compare_airfoils():
     path_str = r"airfoils/all_airfoils/coord_seligFmt"
@@ -32,20 +36,21 @@ def compare_airfoils():
     plt.show()
 
 
-
 # TODO build software to analyse airfoils in rotor design
 
 if __name__ == "__main__":
     # select airfoils from database
-    foil = read_airfoil(r"airfoils/all_airfoils/coord_seligFmt/naca2412.dat")
+    foil = read_airfoil(r"airfoils/naca23018.csv")
     xf.airfoil = foil
     xf.Re = 1e6
     xf.max_iter = 40
-    a, cl, cd, cm, cp = xf.aseq(-5, 20, 2)
+    a, cl, cd, cm, cp = xf.aseq(-20, 20, 0.5)
+    # plt.plot(a, cl)
+    # plt.show()
 
     # TODO make this a function
-    np.save("airfoils/naca2412aero/AoA.npy", a)
-    np.save("airfoils/naca2412aero/cl.npy", cl)
-    np.save("airfoils/naca2412aero/cd.npy", cd)
-    np.save("airfoils/naca2412aero/cm.npy", cm)
-    np.save("airfoils/naca2412aero/cp.npy", cp)
+    np.save("airfoils/naca23018aero/AoA.npy", a)
+    np.save("airfoils/naca23018aero/cl.npy", cl)
+    np.save("airfoils/naca23018aero/cd.npy", cd)
+    np.save("airfoils/naca23018aero/cm.npy", cm)
+    np.save("airfoils/naca23018aero/cp.npy", cp)
