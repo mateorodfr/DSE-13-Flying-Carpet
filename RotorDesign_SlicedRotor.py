142fa4c099e38dee2d0329006bec0f6b77e08c32
wuppii <rocketscienceitis@gmail.com>
2021-05-28 16:32:09 +0200

    Update RotorDesign_SlicedRotor.py

--- RotorDesign_SlicedRotor.py
+++ RotorDesign_SlicedRotor.py
@@ -15,26 +15,30 @@ def main():
     CpA = np.vstack((Cp, AoA)).T
 
     class Blade:
-        def __init__(self, length: float, Nslice: int = 1):
-            self.b       = length
+        def __init__(self, span: float, Nslice: int = 1):
+            self.span    = span
             self.Nslice  = self.setSliceNum(Nslice)
+            self.Lift    = self.getLift()
         
         def setSliceNum(self, Nslice: int) -> None:
             self.Nslice = Nslice
             self.genSlices()
         
         def genSlices(self):
-            self.Slices = [BladeSlice(self.b/N, (self.b/N)*(N-.5), 20) for N in range(1, self.Nslice+1)]
+            self.Slices = [BladeSlice(self.span/self.Nslice, (self.span/self.Nslice)*(N-.5), 15) for N in range(1, self.Nslice+1)]
+
+        def getLift(self) -> float:
+            return np.sum([sl.getLocalLift() for sl in self.Slices])
 
 
     class BladeSlice(Blade):
         def __init__(self, local_dr: float, local_r: float, local_pitch: float = 0):
             self.r     = local_r
             self.dr    = local_dr
-            self.chord = .25
+            self.chord = .25*.5
             self.pitch = local_pitch
 
-            self.ang_vel = 31.666666666666668 # HARD CODED, has to be adapted function
+            self.ang_vel = 130 # HARD CODED, has to be adapted function
 
             self.dL    = self.getLocalCl()
 
@@ -68,18 +72,15 @@ def main():
             
             return Cl * .5 * rho * V * V * S
     
-    ROTOR_BLADE = Blade(2.5, 5)
-    NN = 1
-    print(f"Local Velocity of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalVel()} [m/s]"
-         +f"\nLocal radius of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].r} [m]"
-         +f"\nLocal Thrust of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalLift()} [N]\n\n")
+    ROTOR_BLADE = Blade(2.5/2, 100)
+    NN = 0
+    print(f"\n\n\nLocal v of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalVel()} [m/s]"
+         +f"\nLocal Cl of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalCl()} [-]"
+         +f"\nLocal r of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].r} [m]"
+         +f"\nLocal T of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalLift()} [N]")
 
-
-
-
-
-
-
+             
+    print(f"\nMax Weight equals {ROTOR_BLADE.Lift*4*4/9.80665} [kg]\n\n\n")
 
 
 
