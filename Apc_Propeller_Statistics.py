ddcaa83c7c06a7440cb4b1c2c365d45180bf0451
wuppii <rocketscienceitis@gmail.com>
2021-06-01 09:19:46 +0200

    Update Apc_Propeller_Statistics.py

--- Apc_Propeller_Statistics.py
+++ Apc_Propeller_Statistics.py
@@ -1,5 +1,35 @@
 import os
+import numpy as np
+import pprint
 
-files = os.listdir(r"APC_Propellers\PERFILES_WEB\PERFILES2")
+def is_all_n(s):
+    try:
+        for n in s:
+            float(n)
+        return True
+    except ValueError:
+        return False
 
-print(len(files))
+def contains_NaN(s):
+    newa = []
+    for it in s:
+        try:
+            newa.append(float(it))
+        except:
+            print(it)
+            newa.append(float(it[0:len(it)-4]))
+            newa.append(np.NaN)
+    return newa
+
+pp = pprint.PrettyPrinter(indent=4)
+path = r"APC_Propellers\PERFILES_WEB\PERFILES2\\"
+files = sorted(os.listdir(path))
+
+with open(path+f"{files[0]}") as f:
+    alllines = f.readlines()
+    fileout = [_.strip().split() for _ in alllines][17:]
+
+
+a = [line for line in fileout if line]
+a = [line for i, line in enumerate(a) if (i+1)%31 != 0 and (i+1)%32 != 0 and (i+1)%33 != 0]
+pp.pprint(a)
