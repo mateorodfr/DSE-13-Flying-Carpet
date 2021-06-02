c36ab8576a407c9ac1e2774f204ce19f5df695af
M.C.B. van den Berg <rocketscienceitis@gmail.com>
2021-06-02 08:50:42 +0200

    Update RotorDesign_SlicedRotor.py

--- RotorDesign_SlicedRotor.py
+++ RotorDesign_SlicedRotor.py
@@ -18,7 +18,7 @@ def main():
         def __init__(self, span: float, Nslice: int = 1):
             self.span    = span
             self.Nslice  = self.setSliceNum(Nslice)
-            self.Lift    = self.getLift()
+            self.Thrust    = self.getThrust()
         
         def setSliceNum(self, Nslice: int) -> None:
             self.Nslice = Nslice
@@ -27,8 +27,9 @@ def main():
         def genSlices(self):
             self.Slices = [BladeSlice(self.span/self.Nslice, (self.span/self.Nslice)*(N-.5), 13) for N in range(1, self.Nslice+1)]
 
-        def getLift(self) -> float:
-            return np.sum([sl.getLocalLift() for sl in self.Slices])
+        def getThrust(self) -> float:
+            LD = np.sum([sl.getLocalLift()-sl.getLocalDrag() for sl in self.Slices])
+            return LD
 
 
     class BladeSlice(Blade):
@@ -38,7 +39,7 @@ def main():
             self.chord = .25*.5
             self.pitch = local_pitch
 
-            self.ang_vel = 87.5 # HARD CODED, has to be adapted function
+            self.ang_vel = 95 # HARD CODED, has to be adapted function 1300RPM MAX
 
             self.dL    = self.getLocalCl()
 
@@ -57,6 +58,13 @@ def main():
             """
             closestAoAindex = np.abs(AoA - self.getLocalAoA()).argmin()
             return ClA[closestAoAindex][0]
+
+        def getLocalCd(self) -> float:
+            """
+            Cd depends on the angle of attack
+            """
+            closestAoAindex = np.abs(AoA - self.getLocalAoA()).argmin()
+            return CdA[closestAoAindex][0]
         
         def getLocalArea(self) -> float:
             return self.dr * self.chord
@@ -71,16 +79,29 @@ def main():
             S   = self.getLocalArea()
             
             return Cl * .5 * rho * V * V * S
+
+        def getLocalDrag(self) -> float:
+            """
+            L = Cl * 1/2 * rho * V * V * S
+            """
+            Cd  = self.getLocalCd()
+            rho = 1.225 # HARD CODED, has to be adapted for altitude
+            V   = self.getLocalVel()
+            S   = self.getLocalArea()
+            
+            return Cd * .5 * rho * V * V * S
     
     ROTOR_BLADE = Blade(2.5/2, 10)
     NN = 0
-    print(f"\n\n\nLocal v of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalVel()} [m/s]"
-         +f"\nLocal Cl of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalCl()} [-]"
-         +f"\nLocal r of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].r} [m]"
-         +f"\nLocal T of slice {NN} equals \t {ROTOR_BLADE.Slices[NN].getLocalLift()} [N]")
+    print(f"\n\n\nLocal v of slice {NN} = \t\t\t {ROTOR_BLADE.Slices[NN].getLocalVel()} [m/s]"
+         +f"\nLocal Cl of slice {NN} = \t\t\t {ROTOR_BLADE.Slices[NN].getLocalCl()} [-]"
+         +f"\nLocal r of slice {NN} = \t\t\t {ROTOR_BLADE.Slices[NN].r} [m]"
+         +f"\nGlobal RPM of slice {NN} \u2248 \t\t {int(ROTOR_BLADE.Slices[NN].ang_vel * 60/(2*np.pi))} RPM"
+         +f"\nLocal L/D of slice {NN} = \t\t\t {ROTOR_BLADE.Slices[NN].getLocalLift()/ROTOR_BLADE.Slices[NN].getLocalDrag()} [-]"
+         +f"\nLocal T of slice {NN} = \t\t\t {ROTOR_BLADE.Slices[NN].getLocalLift()-ROTOR_BLADE.Slices[NN].getLocalDrag()} [N]")
 
-             
-    print(f"\nMax Weight equals {ROTOR_BLADE.Lift*8*4/9.80665} [kg]\n\n\n")
+    NNN = 1
+    print(f"\nMax Weight carried by {NNN} blade(s) = \t {ROTOR_BLADE.Thrust*NNN/9.80665} [kg]\n\n\n")
 
 
 
