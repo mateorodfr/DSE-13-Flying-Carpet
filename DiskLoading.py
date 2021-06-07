17cb395c1102cfdfa54f8e2d5ad160ed633cf4aa
mrdegraaf <M.R.deGraaf@student.tudelft.nl>
2021-06-07 16:50:29 +0200

    changes to hs1606 aerodynamics file

--- DiskLoading.py
+++ DiskLoading.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.optimize import root_scalar
 
-rholst = [1.225, 1.0, 0.88]  # kg/m^3
-alst = [343, 336, 326]  # m/s
+rholst = [1.225, 0.923247]  # kg/m^3
+alst = [340.294, 329.174]  # m/s
 MTOW = 2200  # kg
 DiskLoad = np.linspace(100, 1000, 91)  # N/m^2  !!!!
 g = 9.80665
 Cp = 0.05  # normal range between 0.01 and 0.4
 maxrpm_motor = 1300
 
-colors = ["red", "blue", "green"]
+colors = ["red", "green"]
 
 fig, ax1 = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
 
@@ -29,21 +29,21 @@ for rho, a, col in zip(rholst, alst, colors):
     # RPMreq_interp = interp1d(RPMreq, DiskLoad, fill_value="extrapolate")
     # maxdiskload = root_scalar(lambda x: RPMreq_interp(x) - maxrpm_motor, x0=800, x1=1100).root
     # print(maxdiskload)
-    ax1[0].set_xlabel('Disk Loading (kg/m^2)')
     ax1[0].set_ylabel('Power required [kW]')
     ax1[0].plot(DiskLoad/10, Preq/1e3, label=f"rho = {rho}", color=col)
     ax1[0].tick_params(axis='y')
     # ax1[0].axvline(maxdiskload / g, 0.05, 0.95, linestyle='--', color="black")
 
+    ax1[1].set_xlabel('Disk Loading (kg/m^2)')
     ax1[1].set_ylabel('RPM required [rev/min]')
     ax1[1].plot(DiskLoad / g, RPMreq, color=col)
-    ax1[1].plot(DiskLoad / g, (36 * a / np.pi) / Dreq, linestyle='--', label=f"maxrpm {rho}", color=col)
+    ax1[1].plot(DiskLoad / g, (60 * 0.6 * a / np.pi) / Dreq, linestyle='--', label=f"maxrpm {rho}", color=col) # tip speed reaches mach 0.7
     ax1[1].axhline(maxrpm_motor, label="engine limit", linestyle="--", color="cyan")
     # ax1[1].scatter(maxdiskload / g, maxrpm_motor, marker='x', color='black')
 
 ax2 = ax1[0].twinx()  # instantiate a second axes that shares the same x-axis
 
-color = "red"
+color = "blue"
 ax2.set_ylabel('Diameter required [m]', color=color)  # we already handled the x-label with ax1
 ax2.plot(DiskLoad / g, Dreq, color=color)  # , linestyle="--")
 ax2.tick_params(axis='y', labelcolor=color)
