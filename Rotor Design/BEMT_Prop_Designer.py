06539df797f87812df48374fce69b06da965ed86
wuppii <rocketscienceitis@gmail.com>
2021-06-07 21:13:48 +0200

    Rotor Design Update

--- Rotor Design/BEMT_Prop_Designer.py
+++ Rotor Design/BEMT_Prop_Designer.py
@@ -12,8 +12,7 @@ def main():
             coaxial = input("Set coaxial:\t")
             rpm = input("Set RPM:\t")
             v_inf = input("Set v_inf:\t")
-            twist = input("Set twist:\t")
-            dz = input("Set dz:\t")
+            dz = input("Set dz:\t\t")
             
             nblades = input("Set nblades:\t")
             diameter = input("Set diameter:\t")
@@ -32,7 +31,6 @@ def main():
         lines += f"rpm = {rpm}\n"
         lines += f"rpm2 = {rpm}\n"
         lines += f"v_inf = {v_inf}\n"
-        lines += f"twist = {twist}\n"
         lines += f"dz = {dz}\n"
         lines += f"\n"
         lines += f"[rotor]\n"
@@ -40,7 +38,7 @@ def main():
         lines += f"diameter = {diameter}\n"
         lines += f"radius_hub = {radius_hub}\n"
         lines += f"section = " + " ".join([str(section) for _ in range(n)]) + "\n"
-        lines += f"radius = " + " ".join([(str(dr * (_+.5))) for _ in range(n)]) + "\n"
+        lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 2))) for _ in range(n)]) + "\n"
         lines += f"chord = " + " ".join([chord for _ in range(n)]) + "\n"
         lines += f"pitch = " + " ".join([pitch for _ in range(n)]) + "\n"
         lines += f"\n"
@@ -50,7 +48,7 @@ def main():
             lines += f"diameter = {diameter}\n"
             lines += f"radius_hub = {radius_hub}\n"
             lines += f"section = " + " ".join([str(section) for _ in range(n)]) + "\n"
-            lines += f"radius = " + " ".join([(str(dr * (_+.5))) for _ in range(n)]) + "\n"
+            lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 2))) for _ in range(n)]) + "\n"
             lines += f"chord = " + " ".join([chord for _ in range(n)]) + "\n"
             lines += f"pitch = " + " ".join([pitch for _ in range(n)]) + "\n"
             lines += f"\n"
@@ -64,12 +62,14 @@ def main():
         return (Thrust/(rho * D*D*D*D * f * f)), (Thrust/(rho * D*D*D*D*D * f * f * f))
     
     
-    # setINI(10)
+    # setINI(6)
 
 
     solver = Solver(r"C:\Users\marvd\Documents\GitHub\DSE-13-Flying-Carpet\Rotor Design\rot.ini")
-    T,Q,P,dfU,T2,Q2,P2,dfL = solver.run()
+    # df, df_section = solver.run_sweep("rpm", 40, 900, 1300)
+    # pp.pprint(df)
     
+    T,Q,P,dfU,T2,Q2,P2,dfL = solver.run()
     try:
         a1 = solver.turbine_coeffs(T, Q, P)
         a2 = solver.turbine_coeffs(T2, Q2, P2)
@@ -77,7 +77,15 @@ def main():
         a1 = (np.nan, CT_CP(T, float(config["fluid"]["rho"]), float(config["rotor"]["diameter"]), float(config["case"]["rpm"])/60))
         a2 = (np.nan, CT_CP(T2, float(config["fluid"]["rho"]), float(config["rotor2"]["diameter"]), float(config["case"]["rpm2"])/60))
 
-    print(f"\n{(T+T2)*4 / 9.80665} [kg]")
+    print(f"\n{(T+T2)*4 / 9.80665 } [kg]")
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
