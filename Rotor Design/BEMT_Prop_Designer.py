38a9e9e008f3c808ceb548ad363fa3e1bff57ce9
wuppii <rocketscienceitis@gmail.com>
2021-06-08 16:21:53 +0200

    Final Rotor Design
    Final Rotor Design (for sealevel)
    
    Co-Authored-By: mrdegraaf <62094580+mrdegraaf@users.noreply.github.com>

--- Rotor Design/BEMT_Prop_Designer.py
+++ Rotor Design/BEMT_Prop_Designer.py
@@ -3,11 +3,11 @@ from pybemt.solver import Solver
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import pprint
 import configparser
+from scipy.interpolate import interp1d
 
-def main():
-    def setINI(n, *args) -> None:
+
+def setINI(n, *args) -> None:
         if not args:
             coaxial = input("Set coaxial:\t")
             rpm = input("Set RPM:\t")
@@ -38,7 +38,7 @@ def main():
         lines += f"diameter = {diameter}\n"
         lines += f"radius_hub = {radius_hub}\n"
         lines += f"section = " + " ".join([str(section) for _ in range(n)]) + "\n"
-        lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 2))) for _ in range(n)]) + "\n"
+        lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 3))) for _ in range(n)]) + "\n"
         lines += f"chord = " + " ".join([chord for _ in range(n)]) + "\n"
         lines += f"pitch = " + " ".join([pitch for _ in range(n)]) + "\n"
         lines += f"\n"
@@ -48,7 +48,7 @@ def main():
             lines += f"diameter = {diameter}\n"
             lines += f"radius_hub = {radius_hub}\n"
             lines += f"section = " + " ".join([str(section) for _ in range(n)]) + "\n"
-            lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 2))) for _ in range(n)]) + "\n"
+            lines += f"radius = " + " ".join([(str(np.round(dr * (_+.5), 3))) for _ in range(n)]) + "\n"
             lines += f"chord = " + " ".join([chord for _ in range(n)]) + "\n"
             lines += f"pitch = " + " ".join([pitch for _ in range(n)]) + "\n"
             lines += f"\n"
@@ -56,18 +56,17 @@ def main():
         lines += f"rho = {rho}\n"
         lines += f"mu = {mu}"
 
-        with open("rot_sl_OEI.ini.ini", "w+") as f:
+        fpath = os.path.join(os.getcwd(), "08062021rot_v2.ini")
+        with open(fpath, "w+") as f:
             f.write(lines)
-    def CT_CP(Thrust, Power, rho, D, f):
-        return Thrust/(rho * D*D*D*D * f * f), Power/(rho * D*D*D*D*D * f * f * f)
-    
-    
-    # setINI(6)
 
-    # solver = Solver(r"C:\Users\marvd\Documents\GitHub\DSE-13-Flying-Carpet\Rotor Design\rot.ini")
-    solver = Solver(os.path.join(os.getcwd(), r"rot_sl_OEI.ini"))
-    # df, df_section = solver.run_sweep("rpm", 40, 900, 1300)
-    # pp.pprint(df)
+def main() -> None:
+    def CT_CP(Thrust, Power, rho, D, f):
+        return (Thrust/(rho * D*D*D*D * f * f), Power/(rho * D*D*D*D*D * f * f * f))
+    
+    
+    solver = Solver(r"C:\Users\marvd\Documents\GitHub\DSE-13-Flying-Carpet\Rotor Design\08062021rot_v2.ini")
+
     
     T,Q,P,dfU,T2,Q2,P2,dfL = solver.run()
     try:
@@ -77,14 +76,33 @@ def main():
         a1 = (np.nan, *CT_CP(T, P, float(config["fluid"]["rho"]), float(config["rotor"]["diameter"]), float(config["case"]["rpm"])/60))
         a2 = (np.nan, *CT_CP(T2, P2, float(config["fluid"]["rho"]), float(config["rotor2"]["diameter"]), float(config["case"]["rpm2"])/60))
 
-    print(f"\n{(T+T2)*4 / 9.80665} [kg]")
+
+    print(f"\n{(T+T2)*4 / G} [kg]")
     print(f"\n rotor1: Ct = {a1[1]}, Cp = {a1[2]}, Ct/Cp = {a1[1] / a1[2]}")
     print(f"\n rotor2: Ct = {a2[1]}, Cp = {a2[2]}, Ct/Cp = {a2[1] / a2[2]}")
 
 
 
 if __name__ == '__main__':
-    pp = pprint.PrettyPrinter(indent=4)
+    '''
+    For v2 following settings apply for sealevel:
+    1.0g -> rpm = 958
+    1.1g -> rpm = 1003
+    1.8g -> rpm = 1282
+    '''
+
+    G = 9.80665
+
+    ini_file_path = os.path.join(os.getcwd(), "08062021rot_v2.ini")
     config = configparser.ConfigParser()
-    config.read("rot_sl_OEI.ini")
+    config.read(r"C:\Users\marvd\Documents\GitHub\DSE-13-Flying-Carpet\Rotor Design\08062021rot_v2.ini")
+    
+    # setINI(15)
+
     main()
+
+    # print(np.round(np.linspace(0.195, 0.175, 15), 4))
+
+    # interpo = interp1d([0.125, 0.375, 0.625, 0.875, 1.125, 1.375], [10.0, 15.0, 17.0, 15.0, 13.0, 11.0], kind = "cubic", fill_value="extrapolate")
+    # rng = np.asarray([0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.05, 1.15, 1.25, 1.35, 1.45])
+    # print(np.round(interpo(rng), 3))
