d0c97e8447cd474d8948e024e573a8a7a22e7d67
Silvio <peressinisilvio@gmail.com>
2021-06-08 11:38:40 +0200

    Shear stress in rectangular box working

--- InternalStress.py
+++ InternalStress.py
@@ -28,23 +28,50 @@ xs = [-section.w/2,section.w/2,-section.w/2,section.w/2]
 taus = []
 maxtau = []
 
-if section.shape == "square":
-    dxy = 0.001
-    Vymax, Vxmax = 1 , 1
-    xyarr = np.arange(0,section.w * 0.5 +section.h * 0.5 + dxy, dxy)
-    shearVy = (Vymax/section.Ix) * (section.h/2) * section.t_w * (xyarr < section.w/2) * xyarr + \
-    (Vymax/section.Ix) * (section.h/2) * section.t_w * (xyarr >= section.w/2) * (section.w/2) + \
-    (Vymax/section.Ix) * (section.t_h / 2) * 0.5*(xyarr - section.w/2) ** 2 * (xyarr > (section.w/2))
+dxy = 0.001
+Vymax, Vxmax , Tmax = 1 , 1, 1
 
+if section.shape == "square":
+    xyarr = np.arange(0,section.w * 0.5 +section.h * 0.5 + dxy, dxy)
+    tauy = ((Vymax/section.Ix) * (section.h/2) * section.t_w * (xyarr < section.w/2) * xyarr + \
+    (Vymax/section.Ix) * (section.h/2) * section.t_w * (xyarr >= section.w/2) * (section.w/2))/section.t_w + \
+    ((Vymax/section.Ix) * (section.t_h / 2) *( -0.5*(xyarr - section.w/2) ** 2  +section.h*(xyarr - section.w/2)/2 ) * (xyarr > (section.w/2)))/section.t_h
+
+    taux = np.flip(((Vxmax/section.Iy) * (section.w/2) * section.t_h * (xyarr < section.h/2) *xyarr + \
+    ((Vxmax/section.Iy) * (section.w/2) * section.t_h) * (xyarr >= section.h/2)  * section.h/2)/section.t_h + \
+    ((Vxmax/section.Ix) * (section.t_w / 2) * (-0.5*(xyarr - section.h/2) ** 2 + section.w*(xyarr - section.h/2)/2 ) * (xyarr > (section.h/2)))/section.t_w)
+
+    tauz = Tmax / (2*section.Am) * ( (xyarr < (section.w /2)) * (1/section.t_w)   +  (xyarr >= (section.w /2)) * (1/section.t_h)    )
+
+    tau = taux + tauy + tauz
     #shearVx = (Vxmax/section.Iyy) * (section.w/2)*
 
-    fig, ax = plt.subplots(1,1)
-    #ax = ax.ravel()
-    fig.suptitle('Shear stresses around the crossection')
-    print(xyarr,shearVy)
+if section.shape == "circle"
+    xyarr = np.arange(0,section.r * )
 
-    ax.plot(xyarr, shearVy)
-    ax.title.set_text(r"Shear force in y along xy")
-    ax.set_ylabel(r'tauy - Shear force in xy [N]')
-    ax.set_xlabel(r'xy [m]')
-    plt.show()
+
+fig, ax = plt.subplots(2,2)
+ax = ax.ravel()
+fig.suptitle('Shear stresses around the crossection')
+
+ax[0].plot(xyarr, taux)
+ax[0].title.set_text(r"Shear stress caused by Vx")
+ax[0].set_ylabel(r'tauy - Shear flow in xy [N]')
+ax[0].set_xlabel(r'xy [m]')
+
+ax[1].plot(xyarr, tauy)
+ax[1].title.set_text(r"Shear stress caused by Vy")
+ax[1].set_ylabel(r'tauy - Shear flow in xy [N]')
+ax[1].set_xlabel(r'xy [m]')
+
+ax[2].plot(xyarr, tauz)
+ax[2].title.set_text(r"Shear stress caused by T")
+ax[2].set_ylabel(r'tauy - Shear flow in xy [N]')
+ax[2].set_xlabel(r'xy [m]')
+
+ax[3].plot(xyarr, tau)
+ax[3].title.set_text(r"Total shear stress")
+ax[3].set_ylabel(r'tauy - Shear flow in xy [N]')1
+ax[3].set_xlabel(r'xy [m]')
+
+plt.show()
