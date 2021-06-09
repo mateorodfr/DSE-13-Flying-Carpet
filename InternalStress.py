bcc576edfd380867f31f16ae55866307b7280bd8
mateorodfr <62598297+mateorodfr@users.noreply.github.com>
2021-06-09 16:53:42 +0200

    Beam Crossection optimization
    Optimized the beam crossection for the bridge and rotor, this returns the lightest cross section thats still able to handle the loads.

--- InternalStress.py
+++ InternalStress.py
@@ -3,18 +3,6 @@ import numpy as np
 from BridgeDeflection import section #,Nz_int,Mx_int,My_int
 import matplotlib.pyplot as plt
 
-# sigmas = []
-# maxsigma = []
-# for i in range(len(ys)):
-#     sigma_z = Nz_int/section.A + Mx_int/Ixx * ys[i] + (My_int/Iyy) * xs[i]
-#     sigmas.append(sigma_z)
-
-#     if np.sum(np.abs(sigma_z)) > np.sum(np.abs(maxsigma)):
-#         maxsigma =  np.sum(np.abs(sigma_z))
-#         sigma_final = sigma_z
-
-
-
 taus = []
 maxtau = []
 
