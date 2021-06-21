fb9fbfa8d7aabc4c411a2de995e268a5e7023a57
mrdegraaf <M.R.deGraaf@student.tudelft.nl>
2021-06-21 10:12:47 +0200

    plot of Disk Loading

--- DiskLoading.py
+++ DiskLoading.py
@@ -15,7 +15,7 @@ Cp = 0.05  # normal range between 0.01 and 0.4
 maxrpm_motor = 1300
 design_D = 3    # m
 
-colors = ["red", "green"]
+colors = [("darkgray", "dashdot"), ("black", "solid")]    # , ("dimgrey", "dashed")]
 
 fig, ax1 = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
 
@@ -26,7 +26,7 @@ Dreq = np.sqrt(4*Areq/np.pi)
 
 Design_diskload = DiskLoad[np.argmin(np.abs(Dreq - design_D))] / g
 
-for rho, a, col in zip(rholst, alst, colors):
+for rho, a, (col, linestyle) in zip(rholst, alst, colors):
     Preq = TperEngine * np.sqrt(DiskLoad / (2 * rho))
     RPMreq = 60*np.cbrt(Preq / (rho * Cp * Dreq**5))  # rev/min
 
@@ -41,31 +41,34 @@ for rho, a, col in zip(rholst, alst, colors):
           f"maxCp = {maxCp} \n")
 
 
-    # RPMreq_interp = interp1d(RPMreq, DiskLoad, fill_value="extrapolate")
-    # maxdiskload = root_scalar(lambda x: RPMreq_interp(x) - maxrpm_motor, x0=800, x1=1100).root
-    # print(maxdiskload)
     ax1[0].set_ylabel('Power required [kW]')
-    ax1[0].plot(DiskLoad/10, Preq/1e3, label=f"rho = {rho}", color=col)
+    ax1[0].plot(DiskLoad/10, Preq/1e3, label=fr"$\rho$ = {rho} $kg/m^{3}$", color=col, linestyle=linestyle)
     ax1[0].tick_params(axis='y')
-    # ax1[0].axvline(maxdiskload / g, 0.05, 0.95, linestyle='--', color="black")
 
-    ax1[1].set_xlabel('Disk Loading (kg/m^2)')
+    ax1[1].set_xlabel(r'Disk Loading $(kg/m^{2})$')
     ax1[1].set_ylabel('RPM required [rev/min]')
-    ax1[1].plot(DiskLoad / g, RPMreq, color=col)
-    ax1[1].plot(DiskLoad / g, (60 * 0.6 * a / np.pi) / Dreq, linestyle='--', label=f"maxrpm {rho}", color=col) # tip speed reaches mach 0.7
-    ax1[1].axhline(maxrpm_motor, label="engine limit", linestyle="--", color="cyan")
-    # ax1[1].scatter(maxdiskload / g, maxrpm_motor, marker='x', color='black')
+    ax1[1].plot(DiskLoad / g, RPMreq, color=col, linestyle=linestyle)
+    ax1[1].plot(DiskLoad / g, (60 * 0.6 * a / np.pi) / Dreq,
+                label=fr"max RPM @ $\rho$ = {rho}", color=col, linestyle=linestyle)  # tip speed reaches mach 0.6
 
 ax2 = ax1[0].twinx()  # instantiate a second axes that shares the same x-axis
 
-color = "blue"
+color = "dimgrey"
 ax2.set_ylabel('Diameter required [m]', color=color)  # we already handled the x-label with ax1
-ax2.plot(DiskLoad / g, Dreq, color=color)  # , linestyle="--")
-ax2.axvline(Design_diskload, color="black", linestyle="--")
+ax2.plot(DiskLoad / g, Dreq, color=color, linestyle="dashed", label="Diameter")
+ax2.axvline(Design_diskload, color="red", linestyle="--")
 ax2.tick_params(axis='y', labelcolor=color)
 
-ax1[1].axvline(Design_diskload, color="black", linestyle="--")
+ax1[1].axvline(Design_diskload, color="red", linestyle="--")
+ax1[1].fill_between([0, np.max(DiskLoad / g)], maxrpm_motor, np.max(RPMreq), hatch="//", fc="#FF000055",
+                    edgecolor="white", linewidth=4.0, label="engine limit")
+ax1[1].set_ylim(100, np.max(RPMreq))
 ax1[0].legend()
+ax1[1].legend()
+ax2.legend()
+ax1[0].grid(alpha=0.5)
+ax1[1].grid(alpha=0.5)
 
 fig.tight_layout()
+plt.savefig("figures/diskloading")
 plt.show()
